230c001ea08f28c139c8676c67a8c9aaa78c342a
Fidel9 <dobryihomiak@gmail.com>
2021-11-25 01:38:29 +0300

    Initial commit

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class NewTest extends BasicTest{
+    String searchWithAttribute = "//div[@class='page_wrapper']";
+    String getSearchWithText = "//div[text()='Sauce Labs Backpack']";
+    String attributeContains = "//div[contains(@class,'login_password')]";
+    String textText = "//h4[contains(text(),'usernames are:')]";
+
+    @Test
+    public void searcshLocator() {
+        driver.get("https://www.saucedemo.com/inventory.html");
+        driver.findElement(By.xpath(searchWithAttribute));
+        driver.findElement(By.xpath(getSearchWithText));
+        driver.findElement(By.xpath(attributeContains));
+        driver.findElement(By.xpath(textText));
+       /* driver.findElement();
+        driver.findElement();
+        driver.findElement();
+        driver.findElement();
+        driver.findElement();
+        driver.findElement();
+        driver.findElement();*/
+    }
+}
